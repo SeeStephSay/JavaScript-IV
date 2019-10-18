b7f43d6eaa75cd443061de159ad64ffd815fd773
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-10-17 23:13:38 -0500

    Wrote console.logs to check code. Only one issue - listsSubjects returning undefined.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -2,8 +2,8 @@
 class Person {
 	constructor(attributes) {
 		this.name = attributes.name;
-		this.age = attributes.age;
 		this.location = attributes.location;
+		this.age = attributes.age;
 	}
 	speak() {
 		return `Hello, my name is ${this.name}, and I am from ${this.location}.`;
@@ -17,7 +17,7 @@ class Instructor extends Person {
 		this.catchPhrase = instructorAttributes.catchPhrase;
 	}
 	demo(subject) {
-		return `Today, we are learning about ${subject}.`; //Does passing a parameter into a method mean that we don't use the 'this' keyword in the return statement?
+		return `Today, we are learning about ${subject}.`; //Passing a parameter into a method means that we don't use the 'this' keyword in the return statement
 	}
 	grade(student, subject) {
 		return `${student.name} receives a perfect score on ${subject}.`;
@@ -35,10 +35,10 @@ class Student extends Person {
 		this.favSubjects.forEach((subject) => console.log(subject));
 	}
 	PRAssignment(subject) {
-		return `${student.name} has submitted a PR for ${subject}.`;
+		return `${this.name} has submitted a PR for ${subject}.`;
 	}
 	sprintChallenge(subject) {
-		return `${student.name} has begun their Sprint Challenge on ${subject}.`;
+		return `${this.name} has begun their Sprint Challenge on ${subject}.`;
 	}
 }
 
@@ -59,7 +59,7 @@ class TeamLeads extends Instructor {
 // ************************** PERSON OBJECTS *******************************
 const devanee = new Person({
 	name     : 'Devanee Reid',
-	location : 'USA',
+	location : 'The USA',
 	age      : 25,
 });
 
@@ -71,15 +71,19 @@ const abdel = new Person({
 
 const hui = new Person({
 	name     : 'XuHui Zhu',
-	location : 'USA',
+	location : 'The USA',
 	age      : 20,
 });
+
+console.log(devanee.speak());
+console.log('My name is ', hui.name);
+console.log('Name:', abdel.name, 'Location:', abdel.location);
 
 // ************************** INSTRUCTOR OBJECTS *******************************
 
 const brit = new Instructor({
 	name        : 'Brit Hemming',
-	location    : 'USA',
+	location    : 'Canada',
 	age         : 30,
 	favLanguage : 'JavaScript',
 	specialty   : 'JavaScript',
@@ -95,42 +99,62 @@ const christina = new Instructor({
 	catchPhrase : 'Any clarifying questions?',
 });
 
+console.log("Brit's favorite language is ", brit.favLanguage);
+console.log(brit.grade(devanee, 'React'));
+console.log('Christina always asks ', christina.catchPhrase);
+console.log(christina.speak(), 'My specialty is making creative art in ', christina.favLanguage);
+console.log(christina.demo('React'));
+
 // ************************** STUDENT OBJECTS *******************************
 
 const stephanie = new Student({
-	name        : 'Stephanie Butenhof',
-	location    : 'Oklahoma',
-	age         : 33,
-	favLanguage : 'Ruby',
-	specialty   : 'CSS',
-	catchPhrase : 'GIFs are my JAM!',
+	name               : 'Stephanie Butenhof',
+	location           : 'Oklahoma',
+	age                : 33,
+	previousBackground : 'stay-at-home-mom',
+	className          : 'WEB25',
+	favSubjects        : [ 'HTML', 'CSS' ],
 });
 
 const nate = new Student({
-	name        : 'Nathaniel Mosco',
-	location    : 'Georgia',
-	age         : 28,
-	favLanguage : 'JavaScript',
-	specialty   : 'React',
-	catchPhrase : "Yeah, that's normal.",
+	name               : 'Nathaniel Mosco',
+	location           : 'Georgia',
+	age                : 28,
+	previousBackground : 'nurse',
+	className          : 'WEB23',
+	favSubjects        : [ 'React', 'JavaScript', 'C++' ],
 });
+
+console.log(nate.speak());
+console.log(stephanie.listsSubjects()); // *****************NEED TO FIX THIS SINCE IT'S SHOWING AS UNDEFINED!
+console.log(stephanie.PRAssignment('JavaScript-III'));
+console.log(nate.sprintChallenge('Preprocessing-II'));
 
 // ************************** TEAM LEAD OBJECTS *******************************
 
 const mikaela = new TeamLeads({
-	name        : 'Mikaela Currier',
-	location    : 'Georgia',
-	age         : 23,
-	favLanguage : 'Ruby',
-	specialty   : 'JavaScript',
-	catchPhrase : "It's standy time!",
+	name          : 'Mikaela Currier',
+	location      : 'Georgia',
+	age           : 23,
+	favLanguage   : 'Ruby',
+	specialty     : 'JavaScript',
+	catchPhrase   : "It's standy time!",
+	gradClassName : 'WEB20',
+	favInstructor : 'Josh Knell',
 });
 
 const justin = new TeamLeads({
-	name        : 'Justin Trombley',
-	location    : 'New York',
-	age         : 26,
-	favLanguage : 'JavaScript',
-	specialty   : 'JavaScript, React',
-	catchPhrase : "I'm always here if you need me.",
+	name          : 'Justin Trombley',
+	location      : 'New York',
+	age           : 26,
+	favLanguage   : 'JavaScript',
+	specialty     : 'JavaScript, React',
+	catchPhrase   : "I'm always here if you need me.",
+	gradClassName : 'WEB21',
+	favInstructor : 'Dustin Myers',
 });
+
+console.log(mikaela.speak());
+console.log(justin.demo('Constructor Functions'));
+console.log(mikaela.standUp('WEB23-mikaela'));
+console.log(justin.debugsCode(nate, 'React'));
